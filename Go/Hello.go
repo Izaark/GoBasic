110917f13198e4e3a4db19729fab8aa0efca7568
Izaark <izaak00@hotmail.com>
2017-06-06 18:02:37 -0500

    Init

---
+++ Go/Hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+	for i := 1; i < 10+1; i++ {
+		for j := 0; j <=10+1; j++ {
+			
+		}
+		fmt.Println("3", "*", i)
+	}
+
+}
---
+++ Go/ejemplo2.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    fmt.Println("Feliz día", math.Pi)
+    fmt.Printf("Ahora tienes %g problemas.", math.Nextafter(2,1))
+}
+	
